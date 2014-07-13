7c18ba5f5473ac8c13c0a8d980b441cdcff4845d
Sam Gladstone <gladston@umich.edu>
2014-07-12 23:30:32 -0400

    Refactor for clarity

--- app/workers/scraper_worker.rb
+++ app/workers/scraper_worker.rb
@@ -12,11 +12,17 @@ class ScraperWorker
     short_search_url
   end
 
+  def unique_post?(search, url)
+    search.posts.each { |post| return false if post.url == url }
+    true
+  end
+
   def perform(search_id)
     search = Search.find(search_id)
     root_cl = Search::CITIES[search.location][:url]
     doc = Nokogiri::HTML(open(build_search_url(search)))
-    results_count = 0
+    rows_scraped = 0
+    max_rows_scraped = rand(2..5)
 
     doc.css('p.row').each do |row|
       # .text method removes HTML tags
@@ -24,7 +30,7 @@ class ScraperWorker
       title.capitalize! if title
       # Remove dollar sign so as to allow price to be saved as an integer
       price = row.css('span.l2 span.price').text[1..-1].to_i
-      location = row.css('span.pnr small').text
+      location = row.css('span.pnr small').text.capitalize
       # Remove parens from location
       location = location[2..-2].capitalize if location[2..-2]
       date = row.css('span.date').text
@@ -34,24 +40,25 @@ class ScraperWorker
       page_href = row.css('span.pl a').first['href']
       url = Nokogiri::HTML(open(root_cl + page_href))
       # Strip leading and trailing whitespace from body
-      body = url.css('section#postingbody').text.strip!
+      body = url.css('section#postingbody').text.strip
       if url.css('p.mapaddress small a').first
         google_maps_url = url.css('p.mapaddress small a').first['href']
       end
       # Change url var from Nokogiri::HTML::Document to string to store in DB
       url = root_cl + page_href
 
-      @new_post = Post.create({
-        search: search, title: title, price: price,
-        location: location, date: date, page_href: page_href, url: url,
-        body: body, google_maps_url: google_maps_url
-        })
-
-      results_count += 1
-      # Avoid sending an idententical number of get requests each time
-      break if results_count == (rand(2..5))
+      if unique_post?(search, url)
+        @new_post = Post.create({
+          search: search, title: title, price: price,
+          location: location, date: date, page_href: page_href, url: url,
+          body: body, google_maps_url: google_maps_url
+          })
+      end
+      rows_scraped += 1
+      # Avoid sending an identical number of get requests each time
+      break if rows_scraped == max_rows_scraped
       # Wait a random amount of time so they stop throttling me
-      sleep(rand(13.0..29.0))
+      sleep(rand(13.0..47.0))
     end
   end
 end
