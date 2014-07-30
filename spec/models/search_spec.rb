ba4cffadd9d8ecd3fa63f59823c098926a01f836
Sam Gladstone <gladston@umich.edu>
2014-07-30 17:43:53 -0400

    Add unit tests for Search

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../rails_helper'
 
+# User tests pulled from ValidAttribute gem's GitHub page
 describe User do
   it { should have_valid(:email).when('test@test.com', 'test+spam@gmail.com') }
   it { should_not have_valid(:email).when('fail', 123) }
@@ -10,5 +11,29 @@ describe User do
     it { should_not have_valid(:password).when(nil) }
   end
 
-  it { should_not have_valid(:email) }
+  it { should_not have_valid(:email).when(nil) }
+end
+
+describe Search do
+  it { should have_valid(:query).when('a string') }
+  it { should_not have_valid(:query).when(nil) }
+
+  it { should have_valid(:location).when('Boston') }
+  it { should_not have_valid(:location).when(nil) }
+
+  it { should_not have_valid(:user).when(nil) }
+
+  it { should have_valid(:min_price).when(0) }
+  it { should have_valid(:min_price).when(1000) }
+  it { should have_valid(:min_price).when(nil) }
+  it { should_not have_valid(:min_price).when(-1) }
+  it { should_not have_valid(:min_price).when(-1000) }
+  it { should_not have_valid(:min_price).when('a string') }
+
+  it { should have_valid(:max_price).when(0) }
+  it { should have_valid(:max_price).when(1000) }
+  it { should have_valid(:max_price).when(nil) }
+  it { should_not have_valid(:max_price).when(-1) }
+  it { should_not have_valid(:max_price).when(-1000) }
+  it { should_not have_valid(:max_price).when('a string') }
 end
