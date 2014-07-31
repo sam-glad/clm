a4a16262f1a886139f3b0e201b8c44207ba99c5c
Sam Gladstone <gladston@umich.edu>
2014-07-30 22:09:57 -0400

    Allow nil

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,8 @@
 class Image < ActiveRecord::Base
   validates :post, presence: true
-  validates :img_url, presence: true,
+  validates :img_url,
+            presence: true,
+            allow_nil: true,
             format: { with: /\Ahttp:\/\/images.craigslist.org\/\w*_\d*x\d*\.jpg\z/ }
 
   belongs_to :post
