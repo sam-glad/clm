f9cbb883a77b44c084c8be03c0f53171a3df4f9d
Sam Gladstone <gladston@umich.edu>
2014-07-30 21:33:42 -0400

    Add format validation

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,7 @@
 class Image < ActiveRecord::Base
   validates :post, presence: true
   validates :img_url, presence: true
+            format: { with: /\Ahttp:\/\/images.craigslist.org\/\w{17}_\d*x\d*\.jpg\z/ }
 
   belongs_to :post
 end
