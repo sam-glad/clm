c98727932a4f862413189c5ca1cfcb71e2084f11
Sam Gladstone <gladston@umich.edu>
2014-08-01 02:26:21 -0400

    Early tests; unit tests to be finished tomorrow

--- spec/models/craigslist_result_spec.rb
+++ spec/models/craigslist_result_spec.rb
@@ -1,5 +1,31 @@
 require_relative '../rails_helper'
 
 describe CraigslistResult do
+  before(:each) do
+    @index_page = Nokogiri::HTML(open('spec/models/cl_test_index.html'))
+    @first_row = @index_page.css('p.row').first
+    @second_row = @index_page.css('p.row')[1]
+    @first_cl_post = CraigslistResult.new(@first_row, nil, '')
+    @second_cl_post = CraigslistResult.new(@second_row, nil, '')
+  end
+
+  after(:each) do
+    @cl_post = nil
+  end
+
+  it 'should load the title appropriately' do
+    @first_cl_post.title.should == 'Cb drums black w/ cb cymbols'
+    @second_cl_post.title.should == 'Reason refill gold bundle software bundle'
+  end
+
+  it 'should load the price appropriately' do
+    @first_cl_post.price.should == 300
+    @second_cl_post.price.should == 150
+  end
+
+  it 'should load the location appropriately' do
+    @first_cl_post.location.should == nil
+    @second_cl_post.location.should == 'Back bay - boston'
+  end
 
 end
