a3514f1ba61700f87e602928603453d54a24c479
Sam Gladstone <gladston@umich.edu>
2014-08-02 23:24:54 -0400

    Add (busted) check for latitude and longitude

--- spec/models/craigslist_result_spec.rb
+++ spec/models/craigslist_result_spec.rb
@@ -5,12 +5,16 @@ describe CraigslistResult do
     @index_page = Nokogiri::HTML(open('spec/models/cl_test_index.html'))
     @first_row = @index_page.css('p.row').first
     @second_row = @index_page.css('p.row')[1]
+    @third_row = @index_page.css('p.row')[2]
     @first_cl_post = CraigslistResult.new(@first_row, nil, '')
     @second_cl_post = CraigslistResult.new(@second_row, nil, '')
+    @third_cl_post = CraigslistResult.new(@third_row, nil, '')
   end
 
   after(:each) do
-    @cl_post = nil
+    @first_cl_post = nil
+    @second_cl_post = nil
+    @third_cl_post = nil
   end
 
   it 'should load the title appropriately' do
@@ -26,6 +30,16 @@ describe CraigslistResult do
   it 'should load the location appropriately' do
     @first_cl_post.location.should == nil
     @second_cl_post.location.should == 'Back bay - boston'
+  end
+
+  it 'should load the latitude appropriately' do
+    # @first_cl_post.latitude.should == '42.728100'
+    @third_cl_post.latitude.should == nil
+  end
+
+  it 'should load the longitude appropriately' do
+    # @first_cl_post.longitude.should == -70.982200
+    @third_cl_post.longitude.should == nil
   end
 
 end
